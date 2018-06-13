296b241a56164d74280f25578d48d55e307ef47a
Michael A. Matveev <xtreme@rh1.ru>
2018-06-13 11:55:17 +0300

    Bugfixes

--- lib/Graphics.cpp
+++ lib/Graphics.cpp
@@ -3,19 +3,19 @@
 #include "Graphics.h"
 #include "Tools.h"
 #include "config.h"
-
 extern unsigned char MediumNumbers[];
 extern unsigned char IconsFont[];
 extern unsigned char TinyFont[];
 extern unsigned char MedvedFont[];
-
 // CLK, DIN, DC, RST, CE
 LCD5110 display(PIN_CLK, PIN_DIN, PIN_DC, PIN_RST, PIN_CE);
-
-void Graphics::showTuningBox() {
+void Graphics::showTuningBox()
+{
   display.drawRect(10, 10, 74, 35);
-  for (int x = 11; x < 74; x++) {
-    for (int y = 11; y < 34; y++) {
+  for (int x = 11; x < 74; x++)
+  {
+    for (int y = 11; y < 34; y++)
+    {
       display.clrPixel(x, y);
     }
   }
@@ -24,15 +24,18 @@ void Graphics::showTuningBox() {
   display.update();
 }
 
-void Graphics::hideTuningBox() {
+void Graphics::hideTuningBox()
+{
   display.clrScr();
-  resetOldValues();
   displayBasics();
   drawMenu();
+  old_volume = 0;
 }
 
-void Graphics::displayBasics() {
-  switch (menu) {
+void Graphics::displayBasics()
+{
+  switch (menu)
+  {
     case 1 ... 4:
       display.setFont(IconsFont);
       display.print(ICON_MONO, 76, 0);
@@ -44,9 +47,12 @@ void Graphics::displayBasics() {
   }
 }
 
-void Graphics::drawMenuItem(String item) {
-  for (int x = 0; x < 83; x++) {
-    for (int y = 37; y < 48; y++) {
+void Graphics::drawMenuItem(String item)
+{
+  for (int x = 0; x < 83; x++)
+  {
+    for (int y = 37; y < 48; y++)
+    {
       display.setPixel(x, y);
     }
   }
@@ -57,7 +63,8 @@ void Graphics::drawMenuItem(String item) {
   display.update();
 }
 
-void Graphics::resetOldValues() {
+void Graphics::resetOldValues()
+{
   old_strength = -1;
   old_stereo = false;
   old_station = "";
@@ -65,26 +72,33 @@ void Graphics::resetOldValues() {
   old_volume = 0;
 }
 
-void Graphics::toggleBL() {
+void Graphics::toggleBL()
+{
   currentBL = !currentBL;
-  digitalWrite(PIN_BL, currentBL ? HIGH : LOW);
+  digitalWrite(PIN_BL, currentBL? HIGH:LOW);
   drawBLMenu(currentBL);
 }
 
-void Graphics::drawBLMenu(bool on) {
+void Graphics::drawBLMenu(bool on)
+{
   display.setFont(MedvedFont);
-  if (on) {
+  if (on)
+  {
     display.drawRoundRect(9, 12, 36, 24);
-    display.clrRoundRect(39, 12, 72, 24);    
-  } else {
+    display.clrRoundRect(39, 12, 72, 24);
+  }
+  else
+  {
     display.clrRoundRect(9, 12, 36, 24);
     display.drawRoundRect(39, 12, 72, 24);
   }
   display.update();
 }
 
-void Graphics::drawMenu() {  
-  switch (menu) {
+void Graphics::drawMenu()
+{
+  switch (menu)
+  {
     case 1:
       display.clrScr();
       drawMenuItem("Громкость");
@@ -93,13 +107,13 @@ void Graphics::drawMenu() {
       break;
     case 2:
       drawMenuItem("Ручн. настр.");
-      //resetOldValues();
-      //displayBasics();
+      // resetOldValues();
+      // displayBasics();
       break;
     case 3:
       drawMenuItem("Автонастр.");
-      //resetOldValues();
-      //displayBasics();
+      // resetOldValues();
+      // displayBasics();
       break;
     case 4:
       display.clrScr();
@@ -122,28 +136,35 @@ void Graphics::drawMenu() {
       display.print(utf8rus("(C)2018 Tomarevsky K."), 0, 22);
       display.print(utf8rus("Ver. " FW_VERSION), 0, 29);
       drawMenuItem("Прошивка");
-      break;    
+      break;
   }
 }
 
-void Graphics::init() { 
-  display.InitLCD(); 
+void Graphics::init()
+{
+  display.InitLCD();
   currentBL = DEFAULT_BL;
 };
 
 void Graphics::updateState(int strength, bool stereo, int volume,
-                           String freqText, String station) {
-  switch (menu) {
+String freqText, String station)
+{
+  switch (menu)
+  {
     case MENU_VOLUME ... MENU_AUTO:
       bool needUpdate;
       needUpdate = false;
-      if (freqText != old_freqText) {
+      if (freqText != old_freqText)
+      {
         needUpdate = true;
         display.setFont(MediumNumbers);
         display.print("/////", 0, 0);
-        if (freqText.substring(0, 1) == "1") {
+        if (freqText.substring(0, 1) == "1")
+        {
           display.print(freqText.substring(0, 3), 6, 0);
-        } else {
+        }
+        else
+        {
           display.print(freqText.substring(1, 3), 18, 0);
         }
         display.setPixel(44, 14);
@@ -152,40 +173,47 @@ void Graphics::updateState(int strength, bool stereo, int volume,
         display.setPixel(45, 15);
         display.print(freqText.substring(4, 5), 48, 0);
         old_freqText = freqText;
-      }      
-      if (strength != old_strength && checkMillis2(300)) {        
-        display.setFont(IconsFont);        
-        switch (strength) {
+      }
+      if (strength != old_strength && checkMillis2(300))
+      {
+        display.setFont(IconsFont);
+        switch (strength)
+        {
           case 0 ... 2:
-            if (old_strength_idx != 1) {
+            if (old_strength_idx != 1)
+            {
               needUpdate = true;
               old_strength_idx = 1;
             }
             display.print(ICON_SIGNAL_1, 64, 0);
             break;
           case 3 ... 15:
-            if (old_strength_idx != 2) {
+            if (old_strength_idx != 2)
+            {
               needUpdate = true;
               old_strength_idx = 2;
             }
             display.print(ICON_SIGNAL_2, 64, 0);
             break;
           case 16 ... 20:
-            if (old_strength_idx != 3) {
+            if (old_strength_idx != 3)
+            {
               needUpdate = true;
               old_strength_idx = 3;
             }
             display.print(ICON_SIGNAL_3, 64, 0);
             break;
           case 21 ... 35:
-            if (old_strength_idx != 4) {
+            if (old_strength_idx != 4)
+            {
               needUpdate = true;
               old_strength_idx = 4;
             }
             display.print(ICON_SIGNAL_4, 64, 0);
             break;
           case 36 ... 63:
-            if (old_strength_idx != 5) {
+            if (old_strength_idx != 5)
+            {
               needUpdate = true;
               old_strength_idx = 5;
             }
@@ -194,35 +222,44 @@ void Graphics::updateState(int strength, bool stereo, int volume,
         }
         old_strength = strength;
       }
-      if (old_stereo != stereo && checkMillis2(300)) {
+      if (old_stereo != stereo && checkMillis2(300))
+      {
         needUpdate = true;
         display.setFont(IconsFont);
-        if (stereo) {
+        if (stereo)
+        {
           display.print(ICON_STEREO, 76, 0);
-        } else {
+        }
+        else
+        {
           display.print(ICON_MONO, 76, 0);
         }
         old_stereo = stereo;
       }
-      if (old_station != station && checkMillis2(300) && station.length() > 0) {
+      if (old_station != station && checkMillis2(300) && station.length() > 0)
+      {
         display.setFont(TinyFont);
         display.print("          ", 15, 20);
         display.print(station, 15, 20);
         old_station = station;
         needUpdate = true;
       }
-      if (old_volume != volume) {
+      if (old_volume != volume)
+      {
         needUpdate = true;
         display.setFont(IconsFont);
-        for (int i = 0; i < volume; i++) {
+        for (int i = 0; i < volume; i++)
+        {
           display.print(ICON_VOL_PART, 15 + (i * 4), 28);
         }
-        for (int i = volume; i < 15; i++) {
+        for (int i = volume; i < 15; i++)
+        {
           display.print(ICON_BLANK, 15 + (i * 4), 28);
         }
         old_volume = volume;
       }
-      if (needUpdate) {
+      if (needUpdate)
+      {
         /*digitalWrite(PIN_BL, HIGH);
         delay(300);
         digitalWrite(PIN_BL, LOW);*/
@@ -231,13 +268,17 @@ void Graphics::updateState(int strength, bool stereo, int volume,
       }
       break;
     case MENU_SIGNAL:
-      if (checkMillis2(100)) {
+      if (checkMillis2(100))
+      {
         int strengthAbs = strength / 1.8;
         signalCurrent++;
-        if (signalCurrent > 83) {
-          //display.clrScr();
-          for (int x = 9; x < 84; x++) {
-            for (int y = 0; y < 36; y++) {
+        if (signalCurrent > 83)
+        {
+          // display.clrScr();
+          for (int x = 9; x < 84; x++)
+          {
+            for (int y = 0; y < 36; y++)
+            {
               display.clrPixel(x, y);
             }
           }
@@ -246,14 +287,14 @@ void Graphics::updateState(int strength, bool stereo, int volume,
         display.setFont(TinyFont);
         display.print("  ", 0, 30);
         display.printNumI(strength, 0, 30);
-        display.setPixel(signalCurrent, 35-strengthAbs);        
+        display.setPixel(signalCurrent, 35 - strengthAbs);
         display.update();
         delay(50);
-      }      
+      }
       break;
     case MENU_BL:
-      // Nothing to update
-      break;
+    // Nothing to update
+    break;
   }
   display.disableSleep();
 }
